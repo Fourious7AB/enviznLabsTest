2f47cdea18fdb021bafeace1f31e23777e2b78a4
Fourious7AB <786arupbera@gmail.com>
2026-06-10 09:28:21 +0530

    change name

--- src/data/testimonials.js
+++ src/data/testimonials.js
@@ -2,27 +2,27 @@ const testimonials = [
   {
     id: 1,
     image: "/images/testimonial-1.jpg",
-    name: "Rebien"
+    name: "Ayan"
   },
   {
     id: 2,
     image: "/images/testimonial-2.jpg",
-    name: "Alex"
+    name: "Sayen"
   },
   {
     id: 3,
     image: "/images/testimonial-3.jpg",
-    name: "David"
+    name: "Goku"
   },
   {
     id: 4,
     image: "/images/testimonial-4.jpg",
-    name: "Mark"
+    name: "Itachi"
   },
   {
     id: 5,
     image: "/images/testimonial-5.jpg",
-    name: "John"
+    name: "Saitama"
   }
 ];
 
