a61ab38058d04dad63ce7417ad3525f3a1fa862d
Fourious7AB <786arupbera@gmail.com>
2026-06-10 10:47:15 +0530

    hero section video and thumbnail

--- src/components/VideoSection.jsx
+++ src/components/VideoSection.jsx
@@ -1,40 +1,53 @@
+import { useState } from "react";
 import { Play } from "lucide-react";
-import thumbnail from "../assets/images/video-thumbnail.jpg";
+import thumbnail from "../assets/images/video-thumbnail.jpeg";
 
 export default function VideoSection() {
+  const [playVideo, setPlayVideo] = useState(false);
+
   return (
     <div className="mt-20">
-
       <div className="overflow-hidden rounded-2xl border border-white/10 bg-black">
+        <div className="relative h-[450px]">
 
-        <div className="relative aspect-video">
-
-          <img
-            src={thumbnail}
-            alt="Video"
-            className="w-full h-full object-cover opacity-80"
-          />
-
-          <div className="absolute inset-0 bg-black/30" />
-
-          <button className="absolute inset-0 flex items-center justify-center">
-
-            <div className="w-20 h-20 rounded-full border border-white/20 bg-white/10 backdrop-blur-md flex items-center justify-center">
-
-              <Play
-                size={28}
-                fill="white"
-                className="ml-1"
+          {!playVideo ? (
+            <>
+              <img
+                src={thumbnail}
+                alt="Video"
+                className="w-full h-full object-cover "
               />
 
-            </div>
+              <div className="absolute inset-0 bg-black/30" />
 
-          </button>
+              <button
+                onClick={() => setPlayVideo(true)}
+                className="absolute inset-0 flex items-center justify-center"
+              >
+                <div className="w-20 h-20 rounded-full border border-white/20 bg-white/10 backdrop-blur-md flex items-center justify-center">
+                  <Play
+                    size={28}
+                    fill="white"
+                    className="ml-1"
+                  />
+                </div>
+              </button>
+            </>
+          ) : (
+            <video
+              autoPlay
+              controls
+              className="w-full h-full object-cover"
+            >
+              <source
+                src="https://res.cloudinary.com/duf3tdqit/video/upload/v1781067406/vidssave.com_A_day_in_the_life_of_a_sales_manager___See_how_they_run_360P_aurv8a.mp4"
+                type="video/mp4"
+              />
+            </video>
+          )}
 
         </div>
-
       </div>
-
     </div>
   );
 }
